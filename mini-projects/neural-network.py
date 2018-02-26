2ab1a7123c76adaeba049ae5765f42ad2e182755
Jiawen Zhu <zackzhu1@gmail.com>
2018-02-25 21:01:42 -0800

    update weights of perceptron

--- mini-projects/neural-network.py
+++ mini-projects/neural-network.py
@@ -1,4 +1,3 @@
-# 创建感知
 import numpy as np
 
 class Perceptron:
@@ -33,15 +32,38 @@ class Perceptron:
 		outputs. Updates internal weights according to the perceptron training 
 		rule using these values and an optional learning rate, @param eta.
 		"""
+		for X, y_true in zip(values, train):
+			prediction = self.activate(X)
+			error = y_true - prediction
+			weight_update = error * eta * X
+			self.weights += weight_update
 
-def test():
+def testActivate():
 	p1 = Perceptron(np.array([1, 2]), 0.)
 	assert p1.activate(np.array([ 1,-1])) == 0 # < threshold --> 0
 	assert p1.activate(np.array([-1, 1])) == 1 # > threshold --> 1
 	assert p1.activate(np.array([ 2,-1])) == 0 # on threshold --> 0
 
+def testUpdate():
+	def sum_almost_equal(array1, array2, tol=1e-6):
+		return sum(abs(array1 - array2)) < tol
+
+	p1 = Perceptron(np.array([1, 1, 1]), 0)
+	p1.update(np.array([[2, 0, -3]]), np.array([1]))
+	assert sum_almost_equal(p1.weights, np.array([1.2, 1, 0.7]))
+
+	p2 = Perceptron(np.array([1, 2, 3]), 0)
+	p2.update(np.array([[3, 2, 1], [4, 0, -1]]), np.array([0, 0]))
+	assert sum_almost_equal(p2.weights, np.array([0.7, 1.8, 2.9]))
+
+	p3 = Perceptron(np.array([3, 0, 2]), 0)
+	p3.update(np.array([[2, -2, 4], [-1, -3, 2], [0, 2, 1]]), np.array([0, 1, 0]))
+	assert sum_almost_equal(p3.weights, np.array([2.7, -0.3, 1.7]))
+
+
 if __name__ == "__main__":
-	test()
+	testActivate()
+	testUpdate()
 
 
 
